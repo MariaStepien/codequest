359203ddc0dd0d69574075f3d405ffe2456e05ef
Maria Stepien <maria.stepien@vip.interia.pl>
2025-11-16 15:19:25 +0100

    fix for go back button

--- frontend/src/pages/LevelSelectionPage.jsx
+++ frontend/src/pages/LevelSelectionPage.jsx
@@ -67,7 +67,7 @@ export default function LevelSelectionPage() {
                     <h1 className="text-4xl font-extrabold text-blue-400">
                         The journey begins ✨
                     </h1>
-                    <Link to="/" className="text-blue-400 hover:text-blue-500 transition duration-150 flex items-center space-x-2 font-medium">
+                    <Link to="/dashboard" className="text-blue-400 hover:text-blue-500 transition duration-150 flex items-center space-x-2 font-medium">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                         </svg>
